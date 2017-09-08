0d60965a7b2fe3d58465defdffda867f66bf0edf
Administrador <Administrador@Formacion-TOSH4.ipartekaula.com>
2017-09-08 10:40:20 +0200

    Cancion_Naiara

--- src/Cancion.java
+++ src/Cancion.java
@@ -50,11 +50,94 @@ public class Cancion {
 	public String toString() {
 		return "TíTULO=" + this.titulo + ", ARTISTA=" + this.artista + ", DURACIÓN=" + this.duracion;
 	}
+	
+	
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	static ArrayList<Cancion> listaCanciones = new ArrayList<Cancion>();
+	static int opcionMenu = 1;
+	static Scanner sc = new Scanner(System.in);
+	static int numCancion;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ArrayList listaCanciones = new ArrayList();
+		crearLista();
+		opcionMenu = mostrarMenu();
+
+		while (opcionMenu != 5)
+			switch (opcionMenu) {
+			case 1:
+				listarCanciones();
+				mostrarMenu();
+				break;
+			case 2:
+
+				listarCanciones();
+				eliminarCanciones();
+				listarCanciones();
+				mostrarMenu();
+				break;
+			case 3:
+				crearCancion();
+				listarCanciones();
+				mostrarMenu();
+				break;
+
+			case 4:
+				listarCanciones();
+				mostrarDetalle();
+				mostrarMenu();
+				break;
+
+			}
+		mostrarDespedida();
+
+	}
+
+	private static void mostrarDespedida() {
+		// TODO Auto-generated method stub
+		System.out.println("Agur! ");
+		sc.close();
+
+	}
+
+	private static void crearCancion() {
+		// TODO Auto-generated method stub
+		String titulo = "";
+		String artista = "";
+		String duracion = "";
+
+		System.out.println("Título de la canción: ");
+		sc = new Scanner(System.in);
+		titulo = sc.nextLine();
+
+		System.out.println("Artista de la canción: ");
+		sc = new Scanner(System.in);
+		artista = sc.nextLine();
+
+		System.out.println("Duración de la canción: ");
+		sc = new Scanner(System.in);
+		duracion = sc.nextLine();
+
+		listaCanciones.add(new Cancion(titulo, artista, duracion));
+
+	}
+
+	private static void mostrarDetalle() {
+		// TODO Auto-generated method stub
+
+		System.out.println("¿De qué canción quieres ver el detalle?: ");
+		sc = new Scanner(System.in);
+		numCancion = sc.nextInt();
+
+		System.out.println("Canción " + numCancion + ": " + listaCanciones.get(numCancion));
+
+	}
+
+	private static void crearLista() {
+		// TODO Auto-generated method stub
 
 		listaCanciones.add(new Cancion("Wonderful World", "Sam Cooke", "2:05"));
 		listaCanciones.add(new Cancion("Ex's & Oh's", "Elle King", "4:05"));
@@ -67,30 +150,33 @@ public class Cancion {
 		listaCanciones.add(new Cancion("Mrs. Robinson", "Simon & Garfunkel", "3:50"));
 		listaCanciones.add(new Cancion("Malibu", "Miley Cyrus", "3:51"));
 
-		System.out.println("MENU CANCIONES\n_____________________________\n" + "1. Listar canciones\n"
-				+ "2. Eliminar canciones\n" + "3. Crear canciones\n" + "4. Detalle canciones");
+	}
 
-		Scanner sc = new Scanner(System.in);
-		int opcionMenu = 0;
+	private static int mostrarMenu() {
+		// TODO Auto-generated method stub
+		System.out.println("\n\nMENU CANCIONES\n_____________________________\n" + "1. Listar canciones\n"
+				+ "2. Eliminar canciones\n" + "3. Crear canciones\n" + "4. Detalle canciones\n" + "5. Salir");
+
 		opcionMenu = sc.nextInt();
+		return opcionMenu;
 
-		switch (opcionMenu) {
-		case 1:
-			listarCanciones(listaCanciones);
-			break;
+	}
+
+	public static void listarCanciones() {
+		for (int i = 0; i < listaCanciones.size(); i++) {
+			System.out.println("Canción " + i + ": " + listaCanciones.get(i).getTitulo());
+
 		}
 
 	}
 
-	public static void listarCanciones(ArrayList listaCanciones)
-	{
-		for (int i = 0; i < listaCanciones.size(); i++) {
-			System.out.println("Canción " + i + ": " + listaCanciones.get(i));
+	public static void eliminarCanciones() {
+		System.out.println("¿Qué cancion quieres borrar?");
+		sc = new Scanner(System.in);
 
-		}
+		numCancion = sc.nextInt();
+		listaCanciones.remove(numCancion);
 
-		
-		
 	}
 
 }
